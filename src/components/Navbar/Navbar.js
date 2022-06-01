f3c2ad544361bac0f77d29fbb9674fba8e8cdfd7
Gustavo Mendonça <contato.gustavomendonca@gmail.com>
2022-06-01 03:14:38 -0300

    first commit

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import { IconContext } from "react-icons/lib";
+import {
+  Nav,
+  NavbarContainer,
+  NavLogo,
+  NavIcon,
+  MobileIcon,
+  NavMenu,
+  NavItem,
+  NavLinks,
+} from "./Navbar.elements";
+
+const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+
+  return (
+    <>
+      <IconContext.Provider value={{ color: "#fff" }}>
+        <Nav>
+          <NavbarContainer>
+            <NavLogo to="/">
+              <NavIcon />
+              GMendonça
+            </NavLogo>
+            <MobileIcon onClick={handleClick}>
+              {click ? <FaTimes /> : <FaBars />}
+            </MobileIcon>
+            <NavMenu onClick={handleClick} click={click}>
+              <NavItem>
+                <NavLinks to="/">Home</NavLinks>
+              </NavItem>
+
+              <NavItem>
+                <NavLinks to="/about">Sobre</NavLinks>
+              </NavItem>
+
+              <NavItem>
+                <NavLinks to="/services">Serviços</NavLinks>
+              </NavItem>
+
+              <NavItem>
+                <NavLinks to="/projects">Projetos</NavLinks>
+              </NavItem>
+
+              <NavItem>
+                <NavLinks to="/contact">Contato</NavLinks>
+              </NavItem>
+            </NavMenu>
+          </NavbarContainer>
+        </Nav>
+      </IconContext.Provider>
+    </>
+  );
+};
+
+export default Navbar;
